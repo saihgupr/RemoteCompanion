ef5894dab7d4ef0423cffdf074fa678b0a8412d1
saihgupr <121105543+saihgupr@users.noreply.github.com>
2026-01-22 15:20:42 -0500

    Sileo SHA256 Checksums

--- repo_utils/update_repo.sh
+++ repo_utils/update_repo.sh
@@ -9,6 +9,7 @@ echo "📦 Generating Repository Metadata..."
 # 1. Generate Packages
 ./repo_utils/scan_packages.py > ./docs/Packages
 bzip2 -fk ./docs/Packages
+zstd -c19 ./docs/Packages > ./docs/Packages.zst
 
 # 2. Generate Release
 # Copy template
@@ -18,6 +19,12 @@ cp repo_utils/Release docs/Release
 echo "MD5Sum:" >> docs/Release
 printf " $(md5 -q docs/Packages) $(stat -f%z docs/Packages) Packages\n" >> docs/Release
 printf " $(md5 -q docs/Packages.bz2) $(stat -f%z docs/Packages.bz2) Packages.bz2\n" >> docs/Release
+printf " $(md5 -q docs/Packages.zst) $(stat -f%z docs/Packages.zst) Packages.zst\n" >> docs/Release
+
+echo "SHA256:" >> docs/Release
+printf " $(shasum -a 256 docs/Packages | awk '{print $1}') $(stat -f%z docs/Packages) Packages\n" >> docs/Release
+printf " $(shasum -a 256 docs/Packages.bz2 | awk '{print $1}') $(stat -f%z docs/Packages.bz2) Packages.bz2\n" >> docs/Release
+printf " $(shasum -a 256 docs/Packages.zst | awk '{print $1}') $(stat -f%z docs/Packages.zst) Packages.zst\n" >> docs/Release
 
 echo "✅ Repo updated!"
 echo "👉 Enable GitHub Pages for your 'main' branch, pointing to '/' (root) to serve."
